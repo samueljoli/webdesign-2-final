1498b92cf760a0998ee88bcfea62e89addf1b4f7
sjoli <sjoli@xogrp.com>
2021-05-16 18:42:26 -0400

    refactor

--- src/services/geo/index.js
+++ src/services/geo/index.js
@@ -1,66 +1,5 @@
 import {ZIP_CODE_API_APP_KEY, ZIP_CODE_CLIENT_KEY} from "gatsby-env-variables"
 
-const fakeZipRes = {
-    zip_codes: [
-        {
-            zip_code: "33324",
-            distance: 4.636,
-            city: "Fort Lauderdale",
-            state: "FL"
-        },
-        {
-            zip_code: "33322",
-            distance: 1.997,
-            city: "Fort Lauderdale",
-            state: "FL"
-        },
-        {
-            zip_code: "33313",
-            distance: 3.53,
-            city: "Fort Lauderdale",
-            state: "FL"
-        },
-        {
-            zip_code: "33323",
-            distance: 3.421,
-            city: "Fort Lauderdale",
-            state: "FL"
-        },
-        {
-            zip_code: "33351",
-            distance: 0,
-            city: "Fort Lauderdale",
-            state: "FL"
-        },
-        {
-            zip_code: "33319",
-            distance: 3.058,
-            city: "Fort Lauderdale",
-            state: "FL"
-        },
-        {
-            zip_code: "33321",
-            distance: 2.365,
-            city: "Fort Lauderdale",
-            state: "FL"
-        },
-        {
-            zip_code: "33068",
-            distance: 4.323,
-            city: "Pompano Beach",
-            state: "FL"
-        },
-        {
-            zip_code: "33071",
-            distance: 4.53,
-            city: "Pompano Beach",
-            state: "FL"
-        }
-    ],
-};
-
-//const zipCodes = fakeZipRes.zip_codes.map(z => z.zip_code);
-
 class Geo {
     static displayName = "Point";
     static zipCodeApiHost = 'https://www.zipcodeapi.com';
@@ -69,11 +8,9 @@ class Geo {
 
     static async findLocations(zipCode, radius) {
         const hash = {};
-        let state;
-        let zipCodes;
 
         try {
-            const {zipCodeApiHost, zipCodeClientKey, zipCodeApiKey} = this;
+            const {zipCodeApiHost, zipCodeClientKey} = this;
 
             const zipRaw = await fetch(`${zipCodeApiHost}/rest/${zipCodeClientKey}/radius.json/${zipCode}/${radius}/mile`);
 
@@ -85,48 +22,48 @@ class Geo {
 
             console.log(first, '<<< first');
 
-            state = first.state;
-            zipCodes = zipJson.zip_codes.map(z => z.zip_code);
-        } catch (e) {
-            console.log(e, '<<<<<<< ERROR');
-        }
+            const state = first.state;
+            const zipCodes = zipJson.zip_codes.map(z => z.zip_code);
 
-        const test = await fetch(`https://www.vaccinespotter.org/api/v0/states/${state}.json`);
+            const raw = await fetch(`https://www.vaccinespotter.org/api/v0/states/${state}.json`);
 
-        const res = await test.json();
+            const res = await raw.json();
 
-        const {features} = res;
+            const {features} = res;
 
-        features.forEach((feat) => {
-            const featZipCode = feat.properties.postal_code;
-            const featProviderBrandId = feat.properties.provider_brand_id;
-            const {
-                properties: {
-                    url,
-                    address,
-                    provider_brand_name,
-                    appointments_available,
-                }
-            } = feat;
+            features.forEach((feat) => {
+                const featZipCode = feat.properties.postal_code;
+                const featProviderBrandId = feat.properties.provider_brand_id;
+                const {
+                    properties: {
+                        url,
+                        address,
+                        provider_brand_name,
+                        appointments_available,
+                    }
+                } = feat;
 
-            if (zipCodes.includes(featZipCode) && appointments_available) {
-                const hit = hash[featProviderBrandId];
+                if (zipCodes.includes(featZipCode) && appointments_available) {
+                    const hit = hash[featProviderBrandId];
 
-                if (hit) {
-                    hit.locations.push(feat);
-                }
-                else {
-                    hash[featProviderBrandId] = {
-                        providerUrl: url,
-                        providerAddress: address,
-                        providerName: provider_brand_name,
-                        locations: [feat],
+                    if (hit) {
+                        hit.locations.push(feat);
+                    }
+                    else {
+                        hash[featProviderBrandId] = {
+                            providerUrl: url,
+                            providerAddress: address,
+                            providerName: provider_brand_name,
+                            locations: [feat],
+                        }
                     }
                 }
-            }
-        });
+            });
 
-        return hash;
+            return hash;
+        } catch (e) {
+            console.log(e, '<<<<< Error');
+        }
     }
 }
 
