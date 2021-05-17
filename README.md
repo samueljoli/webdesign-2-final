91e73fe1c2caa311669e18bef93e03cfa237a2e5
sjoli <sjoli@xogrp.com>
2021-05-17 09:36:55 -0400

    update states

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# webdesign-2-final
+# Find A Vaccination center
+
+A single page application that will find participating pharmacies nearby given a
+zipcode and radius.
+
+### APIs used
+
+- [ZipCodeApi](www.zipcodeapi.com)
+- [VaccineSpotter](www.vaccinespotter.org)
--- src/components/MatchesPage/index.js
+++ src/components/MatchesPage/index.js
@@ -1,10 +1,69 @@
 import React, {useContext} from 'react';
-import {Row, Col, Container, Card, Button} from "react-bootstrap"
+import {Row, Col, Container, Card, Button, Spinner} from "react-bootstrap"
 import VaccineSpotterContext from '../../contexts/vaccineSpotter';
 import * as styles from './index.module.css';
 
+const Loader = () => {
+    return (
+        <div className={styles['loadcontainer']}>
+            <Spinner className={styles.loader} animation="border" role="status">
+                <span className="sr-only">Loading...</span>
+            </Spinner>
+        </div>
+    );
+};
+
+const NoData = () => {
+    return (
+        <div className={styles.nodata}>
+            <h1>:( Sorry there was no data found</h1>
+            <Button variant="warning" href="/find">Try Again</Button>
+        </div>
+    );
+};
+
 const MatchesPage = () => {
-    const {data, isFetcing, setIsFetching} = useContext(VaccineSpotterContext);
+    const {data, isFetching, setIsFetching} = useContext(VaccineSpotterContext);
+
+    return (
+        <Container>
+            {isFetching ? (<Loader/>) : (
+                <div>
+                    {data ? (Object.entries(data).map((entries) => {
+                        const [key, provider] = entries;
+                        const {
+                            providerName,
+                            providerUrl,
+                            providerAddress,
+                            locations,
+                        } = provider;
+
+                        return (
+                            <>
+                                <h1 className={styles.header}>We found matches for...</h1>
+                                <h1>{providerName}</h1>
+                                    <div className={styles.cards}>
+                                        {locations.map(({properties}) => {
+                                            return (
+                                                <Card key={key} className={styles.card}>
+                                                    <Card.Body>
+                                                        <Card.Title>{`@ ${properties.address} ${properties.city} ${properties.state}`}</Card.Title>
+                                                        <Button className={styles.btn} href={properties.url} variant="success">Schedule Appointment</Button>
+                                                    </Card.Body>
+                                                </Card>
+                                            );
+                                        })}
+                                    </div>
+                            </>
+                        );
+                    })) : (<NoData />)
+                    }
+                </div>
+            )}
+        </Container>
+    );
+
+    /*
     return (
         <Container>
             {data ? (Object.entries(data).map((entries) => {
@@ -15,7 +74,6 @@ const MatchesPage = () => {
                     providerAddress,
                     locations,
                 } = provider;
-                console.log(provider);
                 return (
                     <>
                         <h1 className={styles.header}>We found matches for...</h1>
@@ -37,6 +95,7 @@ const MatchesPage = () => {
             })) : (<p>NO data</p>)}
         </Container>
     );
+    */
 };
 
 export default MatchesPage;
